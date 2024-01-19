09cb68a46ff019a617cc775637fb4366e21a515b
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-01-19 09:15:16 +0900

    [level 0] Title: 두 수의 합, Time: 42.94 ms, Memory: 34.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.js
@@ -0,0 +1,13 @@
+function sum(a, b) {
+    const result = String(BigInt(a) + BigInt(b));
+    
+    return result;
+}
+
+function solution(a, b) {
+    let answer = '';
+    
+    answer = sum(a, b);
+    
+    return answer;
+}
