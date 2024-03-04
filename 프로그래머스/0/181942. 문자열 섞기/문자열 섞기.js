7489b3439e9e8594126c091af307cd1f6a60044c
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-03-05 07:59:58 +0900

    [level 0] Title: 문자열 섞기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -0,0 +1,18 @@
+function stringMix(str1, str2) {
+    let result = '';
+    
+    for (let i = 0; i < str1.length; i++) {
+        result += str1[i];
+        result += str2[i];
+    }
+    
+    return result;
+}
+
+function solution(str1, str2) {
+    let answer = '';
+    
+    answer = stringMix(str1, str2);
+    
+    return answer;
+}
