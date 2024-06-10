1642ac4236b60fb1dbce832722110a0a3a08a079
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-06-10 18:07:17 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -2,13 +2,23 @@ function solution(n) {
     var answer = 0;
     let na="";
 
-    while(n / 3 >= 1){
-        temp = n % 3
-        if(!(temp === 0 && !na)) na += n % 3
+    // while(n / 3 >= 1){
+    //     temp = n % 3
+    //     if(!(temp === 0 && !na)) na += n % 3
+    //     n=Math.floor(n/3)
+    // }
+    
+    while(n/3>=1){
+        na += `${n%3}` //45/3 =15 나머지 0  15/3 = 5 / 3 
         n=Math.floor(n/3)
     }
     
     na += n;
+    let idx = 0;
+    while(true) {
+        if(na[idx] !== "0") break;
+        idx++;
+    }
     
     for(let i = na.length - 1; i >= 0; i--) {
         answer += 3 ** (na.length - 1 - i) * na[i];
