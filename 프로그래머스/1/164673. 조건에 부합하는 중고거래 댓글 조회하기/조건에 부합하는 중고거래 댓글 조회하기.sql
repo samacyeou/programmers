3d8a6f12a08a26a1f3a69a1d335709088ad000ec
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-06-25 15:05:50 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,5 +1,23 @@
-SELECT A.TITLE, A.BOARD_ID, B.REPLY_ID, B.WRITER_ID, B.CONTENTS, TO_CHAR(B.CREATED_DATE, 'YYYY-MM-DD') CREATED_DATE
-FROM USED_GOODS_BOARD A, USED_GOODS_REPLY B
-WHERE A.BOARD_ID = B.BOARD_ID AND TO_CHAR(A.CREATED_DATE, 'YYYY-MM') = '2022-10'
-ORDER BY 6, 1;
-
+-- 코드를 입력하세요
+ # SELECT
+ #     B.TITLE,
+ #     B.BOARD_ID,
+ #     R.REPLY_ID,
+ #     R.WRITER_ID,
+ #     R.CONTENTS,
+ #     DATE_FORMAT(R.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE
+ # FROM USED_GOODS_BOARD AS B
+ #     JOIN USED_GOODS_REPLY AS R
+ #     ON B.BOARD_ID = R.BOARD_ID
+ # WHERE B.CREATED_DATE LIKE '2022-10%'
+ # GROUP BY B.BOARD_ID
+ # ORDER BY R.CREATED_DATE, B.TITLE
+ 
+ 
+ 
+ SELECT ub.title, ub.board_id, 
+ur.reply_id, ur.writer_id, ur.contents, DATE_FORMAT(ur.created_date, "%Y-%m-%d") AS created_date
+FROM used_goods_board ub JOIN used_goods_reply ur
+ON ub.board_id = ur.board_id
+WHERE ub.created_date LIKE "2022-10%"
+ORDER BY ur.created_date ASC, ub.title ASC;
