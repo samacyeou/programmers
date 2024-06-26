97d81b7bae2efe02928beb6bbbfe535d4b323079
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-06-27 06:27:55 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 1.93 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <numeric>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+//한 번의 pop과 한 번의 insert를 합쳐서 작업을 1회
+//어떤 방법으로도 각 큐의 원소 합을 같게 만들 수 없는 경우, -1을 return
+//queue1의 길이 = queue2의 길이
+
+int solution(vector<int> queue1, vector<int> queue2) {
+    int answer = 0;
+    long sum1 = accumulate(queue1.begin(), queue1.end(), 0);
+    long sum2 = accumulate(queue2.begin(), queue2.end(), 0);
+    long goal = (sum1 + sum2) / 2;
+    queue<int> q1;
+    queue<int> q2;
+    for(int i = 0; i < queue1.size(); ++i)
+    {
+        q1.push(queue1[i]);
+        q2.push(queue2[i]);
+        // if(queue1[i] > goal || queue2[i] > goal) return -1;
+    }
+
+
+    while(true)
+    {
+        if(sum1 == goal) return answer;
+
+        else if(sum1 > goal)
+        {
+            int temp = q1.front();
+            q1.pop();
+            q2.push(temp);
+            sum1 -= temp;
+        }
+        else if(sum1 < goal)
+        {
+            int temp = q2.front();
+            q2.pop();
+            q1.push(temp);
+            sum1 += temp;
+        }
+        answer++;
+
+        if(answer > queue1.size() * 3) return -1;
+    }
+}
