808f5237da619f3d13ed5ac304a1f03e9e03427b
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-07-02 18:36:08 +0900

    [level 3] Title: 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -1,18 +1,18 @@
 # (1)[null값을 10으로 치환 후 join하고 평균]
-# select 
-# count(*) as FISH_COUNT, max(f1.length) as max_length, f1.fish_type 
-# from fish_info f1 join (
-#     select id,
-#     case 
-#         when length is null then 10
-#         else length
-#     end as length
-#     from fish_info
-#     ) as f2
-# on f1.id=f2.id
-# group by f1.fish_type 
-# having avg(f2.length)>=33
-# order by f1.fish_type
+select 
+count(*) as FISH_COUNT, max(f2.length) as max_length, f1.fish_type 
+from fish_info f1 join (
+    select id, fish_type,
+    case 
+        when length is null then 10
+        else length
+    end as length
+    from fish_info
+    ) as f2
+on f1.id=f2.id
+group by f1.fish_type
+having avg(f2.length)>=33
+order by f1.fish_type
 
 
 
@@ -29,18 +29,18 @@
 
 
 # (3) avg로 평균 구한 후 join 100점
-select count(*) as FISH_COUNT, max(f1.length) as max_length, f2.fish_type 
-from fish_info f1 join (
-    select fish_type, avg(
-    case 
-        when length is null then 10
-        else length
-    end 
-    ) as avg
-    from fish_info
-    group by fish_type
-    having avg>=33
-    ) as f2
-on f1.fish_type=f2.fish_type
-group by f1.fish_type
-order by f1.fish_type
+# select count(*) as FISH_COUNT, max(f1.length) as max_length, f2.fish_type 
+# from fish_info f1 join (
+#     select fish_type, avg(
+#     case 
+#         when length is null then 10
+#         else length
+#     end 
+#     ) as avg
+#     from fish_info
+#     group by fish_type
+#     having avg>=33
+#     ) as f2
+# on f1.fish_type=f2.fish_type
+# group by f1.fish_type
+# order by f2.fish_type
