421b6e8dc42e3208c98f85a155c9dd864387c2c6
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-07-04 20:21:12 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 137.63 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/0/181950. 문자열 반복해서 출력하기/README.md
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 반복해서 출력하기 - 181950 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181950?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181950?language=java) 
 
 ### 성능 요약
 
-메모리: 32.3 MB, 시간: 42.54 ms
+메모리: 69.5 MB, 시간: 137.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 3일 8:34:1
+2024년 07월 04일 20:21:09
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+
+        if(str.length() >= 1 && str.length() <= 10 && n >= 1 && n <= 5){
+            for(int i = 0; i <= n-1; i++){
+            System.out.print(str);
+            }
+        }
+    sc.close();
+    }
+}
