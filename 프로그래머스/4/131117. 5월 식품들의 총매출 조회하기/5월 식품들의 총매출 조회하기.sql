85e27f0518ee3a4b4ee0969069cd577ceeb7d9a8
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-07-10 06:24:50 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,11 +1,42 @@
 -- 코드를 입력하세요
-SELECT
-    P.PRODUCT_ID AS PRODUCT_ID,
-    P.PRODUCT_NAME AS PRODUCT_NAME,
-    SUM(P.PRICE * O.AMOUNT) AS TOTAL_SALES
-FROM FOOD_PRODUCT AS P
-    JOIN FOOD_ORDER AS O
-        ON P.PRODUCT_ID = O.PRODUCT_ID
-WHERE O.PRODUCE_DATE LIKE '2022-05%'
-GROUP BY PRODUCT_NAME
+# SELECT
+#     P.PRODUCT_ID AS PRODUCT_ID,
+#     P.PRODUCT_NAME AS PRODUCT_NAME,
+#     SUM(P.PRICE * O.AMOUNT) AS TOTAL_SALES
+# FROM FOOD_PRODUCT AS P
+#     JOIN FOOD_ORDER AS O
+#         ON P.PRODUCT_ID = O.PRODUCT_ID
+# WHERE O.PRODUCE_DATE LIKE '2022-05%'
+# GROUP BY PRODUCT_NAME
+# ORDER BY TOTAL_SALES DESC, PRODUCT_ID ASC
+
+
+
+# select
+#     a.PRODUCT_ID,
+#     a.PRODUCT_NAME,
+#     a.TOTAL_SALES
+# from (
+#     select
+#         p.product_id AS PRODUCT_ID,
+#         p.product_name AS PRODUCT_NAME,
+#         p.price * sum(o.amount) TOTAL_SALES,
+#         date_format(o.produce_date,'%Y-%m') bbb
+#     from food_product p
+#         left join food_order o
+#         on p.product_id=o.product_id
+#     group by p.product_id
+#     ) a
+# where a.bbb='2022-05'
+# order by a.TOTAL_SALES desc, a.product_id
+
+
+
+
+
+SELECT A.PRODUCT_ID, A.PRODUCT_NAME, A.PRICE * SUM(B.AMOUNT) AS TOTAL_SALES
+FROM FOOD_PRODUCT A, FOOD_ORDER B
+WHERE A.PRODUCT_ID = B.PRODUCT_ID AND
+B.PRODUCE_DATE LIKE "2022-05%"
+GROUP BY A.PRODUCT_ID
 ORDER BY TOTAL_SALES DESC, PRODUCT_ID ASC
