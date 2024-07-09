88b403ea7e347543adbc9418a292302f79420862
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-07-10 06:58:46 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,9 @@
+import math
+def solution(price):
+    if price >= 100000 and price < 300000:
+        price = price * 0.95
+    elif price >= 300000 and price < 500000:
+        price = price * 0.90
+    elif price >= 500000:
+        price = price * 0.80
+    return math.floor(price)
