9ea47dc11091c62538f22738d094b99eb3bd1bd3
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-07-14 09:19:31 +0900

    [level 1] Title: 모의고사, Time: 4.02 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840#qna) 
 
 ### 성능 요약
 
-메모리: 72.7 MB, 시간: 3.50 ms
+메모리: 77.6 MB, 시간: 4.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 13일 08:20:48
+2024년 07월 14일 09:19:32
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -25,15 +25,14 @@ class Solution {
 
         }
         
+        int fir = 1;
+        int sec = 0;
         int max = map.get(1);
         int count = 1;
-        int first = 1;
-        int sec = 0;
-        
         for(int i = 2; i < 4; i++) {
             if(map.get(i) > max) {
                 max = map.get(i);
-                first = i;
+                fir = i;
                 count = 1;
             } else if(map.get(i) == max) {
                 count++;
@@ -42,48 +41,47 @@ class Solution {
         }
         
         int[] answer;
-        if(count == 3) answer = new int[]{1, 2, 3};
-        else if(count == 2) answer = new int[]{first, sec};
-        else answer = new int[]{first};
-            
-        return answer;    
+        if(count == 3) answer = new int[]{1,2,3};
+        else if(count == 2) answer = new int[]{fir,sec};
+        else answer = new int[]{fir};
 
-//         List<Integer> keySet = new ArrayList<>(map.keySet());
+        List<Integer> keySet = new ArrayList<>(map.keySet());
 
-//         Collections.sort(keySet,new Comparator<Integer>(){
-//             @Override
-//             public int compare(Integer o1, Integer o2){
-//                return map.get(o2)-map.get(o1);   
-//             }
-//         });
+        Collections.sort(keySet,new Comparator<Integer>(){
+            @Override
+            public int compare(Integer o1, Integer o2){
+               return map.get(o2)-map.get(o1);   
+            }
+        });
 
-//         List<Integer> finalList = new ArrayList<>();
-        
-//         if(map.get(keySet.get(0))==map.get(keySet.get(1))
-//            && map.get(keySet.get(1))==map.get(keySet.get(2))){
-//                  finalList.add(keySet.get(0));
-//                  finalList.add(keySet.get(1));
-//                  finalList.add(keySet.get(2));    
+        List<Integer> finalList = new ArrayList<>();
+
+        if(map.get(keySet.get(0))==map.get(keySet.get(1))
+           && map.get(keySet.get(1))==map.get(keySet.get(2))){
+                 finalList.add(keySet.get(0));
+                 finalList.add(keySet.get(1));
+                 finalList.add(keySet.get(2));    
 
 
-//            }
-//            else if(map.get(keySet.get(0))==map.get(keySet.get(1))){
-//                finalList.add(keySet.get(0));
-//                finalList.add(keySet.get(1));
-//            }
-//            else{
-//                finalList.add(keySet.get(0));
-//            }
+           }
+           else if(map.get(keySet.get(0))==map.get(keySet.get(1))){
+               finalList.add(keySet.get(0));
+               finalList.add(keySet.get(1));
+           }
+           else{
+               finalList.add(keySet.get(0));
+           }
 
 
-//            Collections.sort(finalList);
+           Collections.sort(finalList);
 
-//            int arr[]=new int[finalList.size()];
-//            for(int i=0;i<finalList.size();i++){
-//                arr[i]=finalList.get(i);
-//            }
+           int arr[]=new int[finalList.size()];
+           for(int i=0;i<finalList.size();i++){
+               arr[i]=finalList.get(i);
+           }
 
-//            return arr;
+        // return arr;
+           return answer;
 
 
 
