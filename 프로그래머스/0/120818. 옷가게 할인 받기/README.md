83a4d974174364d0276d601e4eb4d100f0ac2a9f
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-08-03 17:57:42 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/README.md
+++ 프로그래머스/0/120818. 옷가게 할인 받기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 옷가게 할인 받기 - 120818 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120818?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120818?language=python3) 
 
 ### 성능 요약
 
-메모리: 74 MB, 시간: 0.03 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 03일 17:56:48
+2024년 08월 03일 17:57:40
 
 ### 문제 설명
 
--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,9 +1,11 @@
 import math
 def solution(price):
-    if price >= 100000 and price < 300000:
-        price = price * 0.95
-    elif price >= 300000 and price < 500000:
-        price = price * 0.90
-    elif price >= 500000:
-        price = price * 0.80
-    return math.floor(price)
+    if price<100000:
+        answer=price
+    elif 100000<=price<300000:
+        answer=price*0.95
+    elif 300000<=price<500000:
+        answer=price*0.90
+    elif price>=500000:
+        answer=price*0.80
+    return int(answer)
