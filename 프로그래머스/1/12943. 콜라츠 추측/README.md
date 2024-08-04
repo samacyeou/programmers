ff6ea2df261a8c1d37fbe28d54e76767bbb977e7
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-08-04 16:40:45 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/README.md
+++ 프로그래머스/1/12943. 콜라츠 추측/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라츠 추측 - 12943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.04 ms
+메모리: 72.5 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 05일 16:09:44
+2024년 08월 04일 16:40:43
 
 ### 문제 설명
 
--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,14 +1,34 @@
 class Solution {
-    public int solution(int num) {
-        int answer = 0;
-        while(num != 1){
-            num = num%2 == 0? num/2 : num*3+1;
-            answer++;
-            if(answer > 400){
-                answer = -1;
-                break;
+    public long solution(long num) {
+        long count = 0;
+        boolean run = true;
+
+        if(num == 1){
+            count = 0;
+            run = false;
+        }else{
+            while(run)
+            {
+                if(num % 2 == 0){
+                    // 짝수인 경우
+                    count+=1;
+                    num = num/2;
+
+                }else if(num == 1){
+                    // 반복문 종료. 결과값 도출
+                    break;
+
+                }else{
+                    count+=1;
+                    num = (num * 3) + 1;
+                }
+                
+                if(count >= 500) {
+                    count = -1;
+                    break;
+                }
             }
         }
-        return answer;
+        return count;
     }
 }
