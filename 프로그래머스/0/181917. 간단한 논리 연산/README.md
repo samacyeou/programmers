e53dc680eca73735601cb0501ad793a7df7eaa7f
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-08-12 17:23:28 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/README.md
+++ 프로그래머스/0/181917. 간단한 논리 연산/README.md
@@ -1,10 +1,10 @@
 # [level 0] 간단한 논리 연산 - 181917 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181917?language=c) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181917#qna) 
 
 ### 성능 요약
 
-메모리: 4.16 MB, 시간: 0.01 ms
+메모리: 4.19 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 12일 17:22:21
+2024년 08월 12일 17:23:27
 
 ### 문제 설명
 
--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.c
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.c
@@ -5,28 +5,29 @@
 bool solution(bool x1, bool x2, bool x3, bool x4) {
     bool answer = true;
 
-    if(x1 || x2 == true)
-    {
-        if(x3 || x4 == true)
-        {
-            answer = true;
-        }
-        else if(x3 || x4 == false)
-        {
-            answer = false;
-        }
-    }
+   if(x1 || x2 == true)
+   {
+       if(x3 || x4 == true)
+       {
+           answer = true;
+       }
+       else if(x3 || x4 == false)
+       {
+           answer = false;
+       }
+   }
     else if(x1 || x2 == false)
     {
         if(x3 || x4 == true)
-        {
-            answer = false;
-        }
-        else if(x3 || x4 == false)
-        {
-            answer = false;
-        }
+       {
+           answer = false;
+       }
+       else if(x3 || x4 == false)
+       {
+           answer = false;
+       }
     }
+
 
     return answer;
 }
