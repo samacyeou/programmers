d833bfc08905fb1b698b05c31d3c96c88709a141
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-08-13 16:38:49 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.c
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.c
@@ -3,31 +3,30 @@
 #include <stdlib.h>
 
 bool solution(bool x1, bool x2, bool x3, bool x4) {
-    bool answer = true;
+   bool answer = true;
 
-   if(x1 || x2 == true)
-   {
-       if(x3 || x4 == true)
-       {
-           answer = true;
-       }
-       else if(x3 || x4 == false)
-       {
-           answer = false;
-       }
-   }
-    else if(x1 || x2 == false)
-    {
-        if(x3 || x4 == true)
-       {
-           answer = false;
-       }
-       else if(x3 || x4 == false)
-       {
-           answer = false;
-       }
-    }
+if(x1 || x2 == true)
+{
+if(x3 || x4 == true)
+{
+answer = true;
+}
+else if(x3 || x4 == false)
+{
+answer = false;
+}
+}
+else if(x1 || x2 == false)
+{
+if(x3 || x4 == true)
+{
+answer = false;
+}
+else if(x3 || x4 == false)
+{
+answer = false;
+}
+}
 
-
-    return answer;
+return answer;
 }
