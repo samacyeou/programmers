13acbf2754c838fb213ea83ad1ce6c0fd87f68ea
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-08-23 12:11:48 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,15 +1,24 @@
--- 코드를 입력하세요
-SELECT
-    U.USER_ID AS USER_ID,
-    U.NICKNAME AS NICKNAME,
-    CONCAT(U.CITY, ' ', U.STREET_ADDRESS1, ' ', U.STREET_ADDRESS2) AS '전체주소',
-    CONCAT(LEFT(U.TLNO,3), '-', MID(U.TLNO,4,4), '-', RIGHT(U.TLNO,4)) AS '전화번호'
-FROM USED_GOODS_USER AS U
-    JOIN (
-        SELECT WRITER_ID, COUNT(WRITER_ID) AS C
-        FROM USED_GOODS_BOARD
-        GROUP BY WRITER_ID
-        HAVING COUNT(WRITER_ID) >= 3
-    ) AS B
-    ON U.USER_ID = B.WRITER_ID
-ORDER BY B.WRITER_ID DESC
+select User_ID, NICKName, 전체주소, 전화번호
+from (SELECT DISTINCT 
+    B.USER_ID AS User_ID, 
+    B.NICKNAME As NICKName, 
+    B.CITY AS city,
+    (B.CITY || ' ' || B.STREET_ADDRESS1 || ' ' || B.STREET_ADDRESS2) AS 전체주소, 
+    SUBSTR(B.TLNO, 1, 3) || '-' || SUBSTR(B.TLNO, 4, 4) || '-' || SUBSTR(B.TLNO, 8, 4) AS 전화번호, 
+    C.writer_post_count AS count 
+FROM 
+    USED_GOODS_BOARD A
+INNER JOIN 
+    USED_GOODS_USER B 
+    ON A.WRITER_ID = B.USER_ID
+INNER JOIN 
+    (SELECT 
+         WRITER_ID, 
+         COUNT(*) OVER (PARTITION BY WRITER_ID) AS writer_post_count
+     FROM 
+         USED_GOODS_BOARD
+    ) C 
+    ON C.WRITER_ID = B.USER_ID
+WHERE 
+    C.writer_post_count >= 3
+order by B.USER_ID DESC)
