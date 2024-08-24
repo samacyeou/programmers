99cf107397b6f090f2e48c341633d67ae7a598be
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-08-24 14:15:44 +0900

    [level 0] Title: 문자열 출력하기, Time: 152.21 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+    String str1 = "Hello";
+        String str2 = "World!";
+        String str3 = str1 + str2;
+
+        System.out.println(a);
+
+    }
+}
