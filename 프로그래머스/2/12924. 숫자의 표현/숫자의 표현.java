093bd6294e8b55c67623d27847cae7593237d78f
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-09-19 18:33:19 +0900

    [level 2] Title: 숫자의 표현, Time: 2.59 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,14 +1,26 @@
 class Solution {
     public int solution(int n) {
-        int answer = 1;
+        int answer = 0;
+        for(int i = 1; i<=n; i+=2){
+            if(n%i==0){
+                if((n/i)*2-1>=i){
+                    answer++;
+                }
+                if((n/i-1)/2>=i){
+                    answer++;
+                }
+            }
+        }
         
+        int test = 1;
         for(int i = 1;i<n-1;i++){
             for(int j = i+1;j<n;j++){
-                if((i+j)*(j-i+1)/2==n) { answer++; break; }
+                if((i+j)*(j-i+1)/2==n) { test++; break; }
                 else if((i+j)*(j-i+1)/2>n) break;
             }
         }
         
-        return answer;
+        System.out.println(test);
+        return test;
     }
 }
