3ebe08e08cab536a38aa19bc733541ea99a8cc43
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-09-25 18:01:50 +0900

    [level 2] Title: 광물 캐기, Time: 36.11 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/README.md
+++ 프로그래머스/2/172927. 광물 캐기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 광물 캐기 - 172927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927?language=cpp) 
 
 ### 성능 요약
 
-메모리: 76.7 MB, 시간: 1.41 ms
+메모리: 4.15 MB, 시간: 36.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 19일 07:35:56
+2024년 09월 25일 18:01:47
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.cpp
@@ -0,0 +1,75 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int toInt(string ingredient)
+{
+    if (ingredient == "diamond")
+        return 0;
+
+    if (ingredient == "iron")
+        return 1;
+
+    if (ingredient == "stone")
+        return 2;
+}
+
+
+
+int dfs(vector<int> picks, vector<vector<int>> fatigueList, int index, int fatigue) 
+{
+    // 곡괭이를 모두 썼거나, 이미 광물을 모두 캔 경우
+    if (picks[0] + picks[1] + picks[2] == 0 || index >= fatigueList.size())
+        return fatigue;
+
+    int minFatigue = 1000;
+
+    for (int k = 0; k < 3; k++)
+    {
+        // k번째 곡괭이를 사용하여 광물을 캐는 경우
+        if (picks[k] > 0)
+        {
+            picks[k]--;
+
+            int tmp = dfs(picks, fatigueList, index + 1, fatigue + fatigueList[index][k]);
+
+            if (minFatigue > tmp)
+                minFatigue = tmp;
+
+            picks[k]++;
+        }
+    }
+
+    return minFatigue;
+
+}
+
+
+int solution(vector<int> picks, vector<string> minerals) {
+    vector<int> intMinerals;
+
+    for (auto mineral : minerals)
+        intMinerals.push_back(toInt(mineral));
+
+    vector<vector<int>> fatigueBoard = { {1,1,1}, {5,1,1}, {25,5,1} };
+
+    vector<vector<int>> fatigueList;
+
+    for (int i = 0; i < intMinerals.size(); i += 5)
+    {
+        vector<int> tmp(3, 0);
+
+        int j = 0;
+        while (j < 5 && i + j < intMinerals.size())
+        {
+            for(int k = 0; k < 3; k++)
+                tmp[k] += fatigueBoard[k][intMinerals[i+j]];
+            j++;
+        }
+
+        fatigueList.push_back(tmp);
+    }
+
+    return dfs(picks, fatigueList, 0, 0);
+}
