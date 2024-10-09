098a4a123255428145762a978419f2817a9632d5
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-10-10 07:36:54 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/README.md
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 자동차 대여 기록 별 대여 금액 구하기 - 151141 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151141?language=mysql) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151141#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 10월 02일 11:58:31
+2024년 10월 10일 07:36:53
 
 ### 문제 설명
 
--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -25,4 +25,49 @@ WHERE
 ORDER BY
     FEE DESC,
     HIS.HISTORY_ID DESC;
+    
+    
+    
+# WITH TABLE1 AS (
+#     SELECT
+#         H.HISTORY_ID,
+#         H.CAR_ID, datediff(H.END_DATE, H.START_DATE) + 1 AS DURATION,
+#         C.DAILY_FEE,
+#         h.start_date,
+#         h.end_date
+#     FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY H
+#         JOIN CAR_RENTAL_COMPANY_CAR C
+#             ON H.CAR_ID = C.CAR_ID
+#     WHERE C.CAR_TYPE = '트럭'
+# ),
 
+# TABLE2 AS (
+#     SELECT
+#         HISTORY_ID,
+#         CAR_ID,
+#         DURATION,
+#         CASE
+#             WHEN DURATION >= 7 AND DURATION < 30 THEN '7일 이상'
+#             WHEN DURATION >= 30 AND DURATION < 90 THEN '30일 이상'
+#             WHEN DURATION >= 90 THEN '90일 이상'
+#             ELSE '미만'
+#         END AS period,
+#         DAILY_FEE
+#     FROM TABLE1
+# )
+
+# SELECT
+#     T.HISTORY_ID,
+#     CASE
+#         WHEN T.period = '7일 이상'
+#             THEN FLOOR((T.DAILY_FEE * T.DURATION * (100 - P.DISCOUNT_RATE) * 0.01))
+#         WHEN T.period = '30일 이상'
+#             THEN FLOOR((T.DAILY_FEE * T.DURATION * (100 - P.DISCOUNT_RATE) * 0.01))
+#         WHEN T.period = '90일 이상'
+#             THEN FLOOR((T.DAILY_FEE * T.DURATION * (100 - P.DISCOUNT_RATE) * 0.01))
+#         ELSE FLOOR(T.DAILY_FEE * T.DURATION)
+#     END AS FEE
+# FROM TABLE2 T
+# JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN P ON T.period = P.DURATION_TYPE
+# WHERE P.CAR_TYPE = '트럭'
+# ORDER BY FEE DESC, T.HISTORY_ID DESC;
