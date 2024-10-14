3e0f2474b501ff91dea55856634c293275b1cc3d
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-10-15 07:26:31 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 75.16 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/README.md
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 파일명 정렬 - 17686 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686#qna) 
 
 ### 성능 요약
 
-메모리: 104 MB, 시간: 66.72 ms
+메모리: 111 MB, 시간: 75.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 14일 14:00:00
+2024년 10월 15일 07:26:31
 
 ### 문제 설명
 
--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -3,51 +3,78 @@ import java.util.Comparator;
 
 class Solution {
     public String[] solution(String[] files) {
-        String[] answer = new String[files.length];
-        
+        String[] answer = {};
+        String[][] parseFile = new String[files.length][3];
+        for(int i=0;i<files.length;i++){
+            String[] result = parse(files[i]);
+            parseFile[i][0] = result[0];
+            parseFile[i][1] = result[1];
+            parseFile[i][2] = result[2];
+        }
+
         Arrays.sort(files, new Comparator<String>() {
             public int compare(String s1, String s2) {
-                String s1s = "";
-                String s2s = "";
-                String s1n = "";
-                String s2n = "";
-                boolean check = true;
-                int count = 0;
-                
-                for(int i = 0; i < s1.length(); i++) {
-                    if(s1.charAt(i) >= '0' && s1.charAt(i) <= '9') {
-                        check = false;
-                        s1n += s1.charAt(i);
-                        count++;
-                        if(count == 5) break;
-                    } else {
-                        if(check) s1s += Character.toLowerCase(s1.charAt(i));
-                        else break;
-                    }
-                }
-                
-                check = true;
-                count = 0;
-                
-                for(int i = 0; i < s2.length(); i++) {
-                    if(s2.charAt(i) >= '0' && s2.charAt(i) <= '9') {
-                        check = false;
-                        s2n += s2.charAt(i);
-                        count++;
-                        if(count == 5) break;
-                    } else {
-                        if(check) s2s += Character.toLowerCase(s2.charAt(i));
-                        else break;
-                    }
-                }
-                
-                if(s1s.equals(s2s)) return Integer.parseInt(s1n) - Integer.parseInt(s2n);
-                else return s1s.compareTo(s2s);
+                String head1 = parse(s1.toLowerCase())[0];
+                String head2 = parse(s2.toLowerCase())[0];
+                if (head1.equals(head2)) {
+                    int num1 = Integer.parseInt(parse(s1)[1]);
+                    int num2 = Integer.parseInt(parse(s2)[1]);
+                    return num1 - num2;
+                } else return head1.compareTo(head2);
             }
         });
-        
-        for(int i = 0; i < files.length; i++) answer[i] = files[i];
-        
-        return answer;
+            
+        // for(int i=0;i<parseFile.length-1;i++){
+        //     for(int j=i+1;j<parseFile.length;j++){
+        //         String head1 = parseFile[i][0].toLowerCase();
+        //         String head2 = parseFile[j][0].toLowerCase();
+        //         if (head1.equals(head2)) {
+        //             int num1 = Integer.parseInt(parseFile[i][1]);
+        //             int num2 = Integer.parseInt(parseFile[j][1]);
+        //             if (num1 > num2) {
+        //                 swap(parseFile, i, j);
+        //             }
+        //         } else if (head1.compareTo(head2) > 0) {
+        //             swap(parseFile, i, j);
+        //         }
+        //     }
+        // }
+        // answer = new String[files.length];
+        // for (int i = 0; i < parseFile.length; i++) {
+        //     answer[i] = parseFile[i][0] + parseFile[i][1] + parseFile[i][2];
+        // }
+
+        // return answer;
+            return files;
+    }
+
+
+    public String[] parse(String file){
+        String head = "";
+        String number = "";
+        String tail = "";
+        int i = 0;
+
+        while (i < file.length() && !('0' <= file.charAt(i) && file.charAt(i) <= '9')) {
+            head += file.charAt(i);
+            i++;
+        }
+
+        while (i < file.length() && ('0' <= file.charAt(i) && file.charAt(i) <= '9') && number.length() < 5) {
+            number += file.charAt(i);
+            i++;
+        }
+
+        if (i < file.length()) {
+            tail = file.substring(i);
+        }
+
+        return new String[]{head, number, tail};
+    }
+    public void swap(String[][] arr , int i , int j){
+        String[] temp = new String[3];
+        for(int k = 0; k < 3; k++) temp[k] = arr[i][k];
+        for(int k = 0; k < 3; k++) arr[i][k] = arr[j][k];
+        for(int k = 0; k < 3; k++) arr[j][k] = temp[k];
     }
 }
