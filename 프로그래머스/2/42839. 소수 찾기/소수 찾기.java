72aff9c3a3a0ad88808fd43b9898679f053367fd
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-10-14 11:28:05 +0900

    [level 2] Title: 소수 찾기, Time: 13.53 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+class Solution {
+    public static Set<Integer> hashSet = new HashSet<>();
+    public static boolean[] visited;
+    public int solution(String numbers) {
+        // 자료구조 선언
+        StringBuilder sb = new StringBuilder();
+        // 자료구조 채우기
+        visited = new boolean[numbers.length()];
+        // 자료구조 초기화
+
+        // 알고리즘
+        dfs(sb, 0, numbers);
+        return hashSet.size();
+    }
+
+    public static void dfs(StringBuilder sb, int k, String numbers){
+        // 소수판별
+        addPrime(sb);
+
+        // 탈출
+        if(k == numbers.length()) {
+            return;
+        }
+
+        // 유배
+        for(int i = 0; i < numbers.length(); i++) {
+            if(visited[i]) {
+                continue;
+            }
+            // 만약 sb가 비어있는데 charat이 0이면 넘어가자 => 이부분을 추가하면 왜 안되는거지?
+            // if(sb.length() == 0 && numbers.charAt(i) == '0') {
+            //     continue;
+            // }
+
+            visited[i] = true;
+            dfs(sb.append(numbers.charAt(i)), k + 1, numbers);
+            sb.deleteCharAt(k);
+            visited[i] = false;
+        }
+    }
+
+    public static void addPrime(StringBuilder sb) {
+        if(sb.length() == 0) {
+            return;
+        }
+
+        // sb를 정수로 전환
+        int num = Integer.parseInt(sb.toString());
+
+        if(num <= 1) {
+            return;
+        }
+
+        int max_value = (int) Math.sqrt(num);
+
+        for(int i = 2; i <= max_value; i++) {
+            if(num % i == 0) {
+                return;
+            }    
+        }
+        hashSet.add(num);
+    }
+}
