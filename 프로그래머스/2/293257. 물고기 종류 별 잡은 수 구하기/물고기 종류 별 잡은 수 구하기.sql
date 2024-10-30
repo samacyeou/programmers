6d234afa0c9bdcf5f7a4710afd194ce681a134ac
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-10-30 11:42:30 +0900

    [level 2] Title: 물고기 종류 별 잡은 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
+++ 프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
@@ -38,24 +38,50 @@ with type_count as (
             on f1.fish_type = f2.fish_type
     group by fish_name
     order by fish_count desc
-)
-
-# select 
-#     answer.fish_count as fish_count, answer.fish_name as fish_name
-#     # case
-#     #     when result.count = 2 then 1
-#     #     when result.count = 1 then answer.fish_count
-#     # end as fish_count,
-#     # case
-#     #     when result.count = 2 then 1
-#     #     when result.count = 1 then answer.fish_name
-#     # end as fish_name
-# from answer
-# order by fish_count desc
-
-select count(*) as fish_count, f2.fish_name as fish_name
-    from fish_info as f1
-        join fish_name_info as f2
+), test_fish_info as (
+    select 0 as ID, 0 as fish_type
+    union
+    select 1 as ID, 1 as fish_type
+), test_fish_name_info as (
+    select 0 as fish_type, "A" as fish_name
+    union
+    select 1 as fish_type, "A" as fish_name
+), test_result as (
+    select count(*) as fish_count, fish_name
+    from test_fish_info as f1
+        join test_fish_name_info as f2
             on f1.fish_type = f2.fish_type
     group by fish_name
     order by fish_count desc
+), test_test as (
+    select count(*) as count
+    from (
+        select count(*) as count
+        from (
+            select count(*) as count
+            from test_fish_info
+            group by fish_type
+        ) as test_test_type_count
+        union
+        select count(*) as count
+        from (
+            select count(*) as count
+            from test_fish_info as ft1
+                join test_fish_name_info as ft2
+                    on ft1.fish_type = ft2.fish_type
+            group by fish_name
+        ) as test_test_name_count
+    ) as test_test_result
+)
+
+select 
+    # answer.fish_count as fish_count, answer.fish_name as fish_name
+    case
+        when result.count = 2 then 0
+        when result.count = 1 then answer.fish_count
+    end as fish_count,
+    case
+        when result.count = 2 then 0
+        when result.count = 1 then answer.fish_name
+    end as fish_name
+from answer, result
