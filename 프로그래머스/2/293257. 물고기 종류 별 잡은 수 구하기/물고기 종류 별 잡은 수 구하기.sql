a96a2a19c4e2dac2e444e33a158696d359594c02
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-10-30 11:21:11 +0900

    [level 2] Title: 물고기 종류 별 잡은 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
+++ 프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
@@ -1,5 +1,37 @@
 -- 코드를 작성해주세요
-with answer as (
+# select count(*) as fish_count, f2.fish_name as fish_name
+# from fish_info as f1
+#     join fish_name_info as f2
+#         on f1.fish_type = f2.fish_type
+# group by fish_name
+# order by fish_count desc
+
+with type_count as (
+    select count(*) as count
+    from (
+        select count(id) as count
+        from fish_info
+        group by fish_type
+    ) as test
+), name_count as (
+    select count(*) as count
+    from (
+        select count(id) as count
+        from fish_info as f1
+            join fish_name_info as f2
+                on f1.fish_type = f2.fish_type
+        group by fish_name
+    ) as test
+), result as (
+    select count(*) as count
+    from (
+        select *
+        from type_count
+        union
+        select *
+        from name_count
+    ) as test
+), answer as (
     select count(*) as fish_count, f2.fish_name as fish_name
     from fish_info as f1
         join fish_name_info as f2
@@ -8,15 +40,22 @@ with answer as (
     order by fish_count desc
 )
 
-select *
-from answer
-
-
-# select fish_count, fish_name
-# from fish_name_info a join (
-#         select fish_type,count(*) as fish_count
-#         from fish_info
-#         group by fish_type
-#     )b
-# using (fish_type)
+# select 
+#     answer.fish_count as fish_count, answer.fish_name as fish_name
+#     # case
+#     #     when result.count = 2 then 1
+#     #     when result.count = 1 then answer.fish_count
+#     # end as fish_count,
+#     # case
+#     #     when result.count = 2 then 1
+#     #     when result.count = 1 then answer.fish_name
+#     # end as fish_name
+# from answer
 # order by fish_count desc
+
+select count(*) as fish_count, f2.fish_name as fish_name
+    from fish_info as f1
+        join fish_name_info as f2
+            on f1.fish_type = f2.fish_type
+    group by fish_name
+    order by fish_count desc
