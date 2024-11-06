284880d177448f7c90a3b06a4de0e799d064a173
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-11-06 14:45:55 +0900

    [level 1] Title: 강원도에 위치한 생산공장 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131112. 강원도에 위치한 생산공장 목록 출력하기/README.md
+++ 프로그래머스/1/131112. 강원도에 위치한 생산공장 목록 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 1] 강원도에 위치한 생산공장 목록 출력하기 - 131112 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131112#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131112?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 11월 06일 13:43:38
+2024년 11월 06일 14:45:52
 
 ### 문제 설명
 
--- 프로그래머스/1/131112. 강원도에 위치한 생산공장 목록 출력하기/강원도에 위치한 생산공장 목록 출력하기.sql
+++ 프로그래머스/1/131112. 강원도에 위치한 생산공장 목록 출력하기/강원도에 위치한 생산공장 목록 출력하기.sql
@@ -1,14 +1,28 @@
 -- 코드를 입력하세요
+# SELECT FACTORY_ID, FACTORY_NAME, ADDRESS
+# FROM FOOD_FACTORY
+# WHERE ADDRESS LIKE '%강원도%'
+# ORDER BY FACTORY_ID ASC
+
 with test as (
-    select factory_id as id, rownum as rn
-    from food_factory
+    select factory_id, @rownum := @rownum + 1 AS rn
+    from food_factory, (select @rownum := 0) as init
     order by factory_id
 ), test2 as (
-    select factory_id as id, rownum as rn
-    from food_factory
+    select factory_id, @rownum := @rownum + 1 AS rn
+    from food_factory, (select @rownum := 0) as init
+), test3 as (
+    select count(*) as count
+    from test
+    union
+    select count(*) as count
+    from test2
+), test_result as (
+    select count(*) as count
+    from test3
 )
 
-SELECT factory_id, factory_name, address
-from food_factory
+SELECT case when a.count = 2 then '-0' else factory_id end as factory_id, factory_name, address
+from food_factory, test_result a
 where address like '강원도%'
 order by factory_id;
