25a63be3b71723684d256fee68f9a58523d24271
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-11-07 14:54:22 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.js
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.js
@@ -1,14 +1,17 @@
-function findLongPart(myString, pat) {
-    const lastIndex = myString.lastIndexOf(pat);
-    const result = myString.slice(0, lastIndex + pat.length);
-    
-    return result;
+const validate = (myString, pat) => {
+    const limit1 = myString.length >= 5 && myString.length <= 20;
+    // const limit2 = pat.length >= 1 && pat.length <= 5;
+    // return limit1 && limit2;
+    return limit1
 }
-
 function solution(myString, pat) {
-    let answer = '';
-    
-    answer = findLongPart(myString, pat);
-    
+    var answer = '';
+    if (validate(myString, pat)) {
+        const strSplit = myString.split(pat);
+        strSplit.forEach((el,i) => {
+            if (i !== strSplit.length-1) answer += el + pat
+        })
+
+    }
     return answer;
 }
