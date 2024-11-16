e6d6875607350e4a7a1ca0af0ed87ca61826c993
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-11-17 07:29:10 +0900

    [level 2] Title: 물고기 종류 별 잡은 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
+++ 프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
@@ -6,91 +6,32 @@
 # group by fish_name
 # order by fish_count desc
 
-with type_count as (
+with test as (
     select count(*) as count
-    from (
-        select count(id) as count
-        from fish_info as f1
-            join fish_name_info as f2
-                on f1.fish_type = f2.fish_type
-        group by f1.fish_type
-    ) as test
-), name_count as (
-    select count(*) as count
-    from (
-        select count(id) as count
-        from fish_info as f1
-            join fish_name_info as f2
-                on f1.fish_type = f2.fish_type
-        group by fish_name
-    ) as test
-), result as (
-    select count(*) as count
-    from (
-        select *
-        from type_count
-        union
-        select *
-        from name_count
-    ) as test
-), answer as (
-    select count(*) as fish_count, f2.fish_name as fish_name
-    from fish_info as f1
-        join fish_name_info as f2
-            on f1.fish_type = f2.fish_type
-    group by fish_name
-    order by fish_count desc
-),
-
-
-
-
-test_fish_info as (
-    select 0 as ID, 0 as fish_type
+    from fish_info as info
+        left join fish_name_info as name
+            on info.fish_type = name.fish_type
     union
-    select 1 as ID, 1 as fish_type
-), test_fish_name_info as (
-    select 0 as fish_type, "A" as fish_name
-    union
-    select 1 as fish_type, "A" as fish_name
+    select count(*) as count
+    from fish_info as info
+        join fish_name_info as name
+            on info.fish_type = name.fish_type
 ), test_result as (
-    select count(*) as fish_count, fish_name
-    from test_fish_info as f1
-        join test_fish_name_info as f2
-            on f1.fish_type = f2.fish_type
-    group by fish_name
-    order by fish_count desc
-), test_test as (
     select count(*) as count
-    from (
-        select count(*) as count
-        from (
-            select count(*) as count
-            from test_fish_info
-            group by fish_type
-        ) as test_test_type_count
-        union
-        select count(*) as count
-        from (
-            select count(*) as count
-            from test_fish_info as ft1
-                join test_fish_name_info as ft2
-                    on ft1.fish_type = ft2.fish_type
-            group by fish_name
-        ) as test_test_name_count
-    ) as test_test_result
+    from test
+), result as (
+    select fish_type, fish_name, count
+    from fish_name_info, test_result
 )
 
-
-
-select 
-    # answer.fish_count as fish_count, answer.fish_name as fish_name
-    case
-        when result.count = 2 then 0
-        when result.count = 1 then answer.fish_count
-    end as fish_count,
-    case
-        when result.count = 2 then 0
-        when result.count = 1 then answer.fish_name
-    end as fish_name
-from answer, result
+SELECT FI.FISH_COUNT
+     , if(fni.count = 1, FNI.FISH_NAME, "BYK") as FISH_NAME
+  FROM (
+       SELECT FI.FISH_TYPE
+            , COUNT(*) FISH_COUNT
+         FROM FISH_INFO FI
+        GROUP BY FI.FISH_TYPE
+       ) FI
+  left JOIN result FNI
+    ON FI.FISH_TYPE = FNI.FISH_TYPE
+ ORDER BY FI.FISH_COUNT DESC
