42127d071e90fd11fe7f488e1dc94b099f3ddf27
samacyeou <151595973+samacyeou@users.noreply.github.com>
2024-11-21 12:00:27 +0900

    [level 2] Title: 올바른 괄호, Time: 0.48 ms, Memory: 3.86 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -9,13 +9,12 @@ bool solution(string s)
     bool answer = true;
     stack<char> stk;
     for(int i=0; i<s.size(); i++){
-        if(s[i] == ')' && stk.empty()) return false;
         if(s[i] == '('){
             stk.push(s[i]);
         }
 
         else{
-            if(stk.top() == '('){ 
+            if(!stk.empty() && stk.top() == '('){ 
                 stk.pop();
             }
             else{
