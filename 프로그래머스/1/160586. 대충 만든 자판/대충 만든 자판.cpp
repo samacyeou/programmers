d6151c26e5ce242c494d7abba69f03e615d6f909
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-01-03 14:07:47 +0900

    [level 1] Title: 대충 만든 자판, Time: 40.93 ms, Memory: 3.7 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.cpp
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.cpp
@@ -1,48 +1,49 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 vector<int> solution(vector<string> keymap, vector<string> targets) {
-    vector<int> answer;
-    vector<char> str;
+    vector<int> answer(targets.size(), 0);
+    int maxSize = 0;
 
-    for(int i=0; i<targets.size(); i++)
-        answer.push_back(0);
 
-    for(int i=0; i<keymap.size(); i++)
+    for(string str : keymap)
     {
-        for(int j=0; j<keymap[i].size(); j++)
-        {
-            auto it = find(str.begin(), str.end(), keymap[i][j]);
-            if(it != str.end())
-            {   // 원소가 이미 존재할 경우
-                if((j + 1) < str[it - str.begin() + 1])
-                    str[it - str.begin() + 1] = (j + 1);
-                continue;
-            }
-            str.push_back(keymap[i][j]);
-            str.push_back((j + 1));
-        }
+        maxSize = str.size() > maxSize ? str.size() : maxSize;
     }
 
-    for(int i=0; i<targets.size(); i++)
+    for(int index = 0; index < targets.size(); index++)
     {
-        for(int j=0; j<targets[i].size(); j++)
+        for(int k = 0; k < targets[index].size(); k++)
         {
-            auto it = find(str.begin(), str.end(), targets[i][j]);
-            if(it != str.end())
+            bool find = false;
+
+            for(int i = 0; i < maxSize; i++)
             {
-                answer[i] += str[it - str.begin() + 1];
-            }
-            else {
-                answer[i] = -1;
-                break;
+                if(find)
+                    break;
+                for(int j = 0; j < keymap.size(); j++)
+                {
+                    if(find)
+                        break;
+                    if(keymap[j].size() <= i) continue;
+                    if(keymap[j][i] == targets[index][k])
+                    {
+                        if(answer[index] == -1)
+                            break;
+                        answer[index] += i + 1;
+                        find = true;
+                    }
+
+                }
+                if(i == maxSize - 1 && find == false)
+                    answer[index] = -1;
             }
         }
+
     }
+
 
     return answer;
 }
