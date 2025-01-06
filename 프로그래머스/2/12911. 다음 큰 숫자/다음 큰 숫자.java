17ad5ded954b7a4a280455f13a39ac7d9d3994d7
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-01-06 10:48:37 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.12 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,23 +1,33 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
         String temp = Integer.toBinaryString(n);
-        int count = 0;
-        
-        for(int i = 0;i<temp.length();i++)
-            if(temp.charAt(i)=='1') answer++;
-        
-        while(true){
-            temp=Integer.toBinaryString(++n);
-            
-            for(int i = 0;i<temp.length();i++)
-                if(temp.charAt(i)=='1') count++;
-            
-            if(count==answer) break;
-            count=0;
+        boolean check = false;
+        int answer = 0;
+        int idx = 0;
+        int end = temp.length();
+
+        for(int i = temp.length() - 1; i >= 0; i--) {
+            if(check && temp.charAt(i) == '0') {
+                idx = i;
+                break;
+            } else if(!check && temp.charAt(i) == '1') {
+                check = true;
+                end = i;
+            }
         }
         
-        answer=n;
+        StringBuilder result = new StringBuilder();
+        
+        
+        result.append(temp.substring(0, idx)).append("10");
+        
+        for(int i = 0; i < temp.length() - end - 1; i++) result.append("0");
+
+        
+        if(idx == 0) result.append("1".repeat(end - idx));
+        else result.append("1".repeat(end - idx - 1));
+        
+        answer = Integer.valueOf(result.toString(), 2);
         
         return answer;
     }
