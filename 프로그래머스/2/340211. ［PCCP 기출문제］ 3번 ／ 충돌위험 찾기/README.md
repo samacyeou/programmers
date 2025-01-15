bc9348e0887d07d24fa703659bbccdab9de5e52f
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-01-15 09:38:44 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 648.54 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/README.md
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 3번 / 충돌위험 찾기 - 340211 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340211?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340211#qna) 
 
 ### 성능 요약
 
-메모리: 329 MB, 시간: 566.31 ms
+메모리: 83.9 MB, 시간: 648.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 09일 09:30:05
+2025년 01월 15일 09:38:45
 
 ### 문제 설명
 
--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
@@ -1,51 +1,57 @@
+from collections import Counter
+def route_list(start_r,start_c, end_r,end_c, route_lst):
+    while not(start_r==end_r and start_c==end_c):
+
+        if start_r<end_r:
+            start_r+=1
+            route_lst.append((start_r,start_c))
+            continue
+
+        if start_r>end_r:
+            start_r-=1
+            route_lst.append((start_r,start_c))
+            continue
+        if start_c<end_c:
+            start_c+=1
+            route_lst.append((start_r,start_c))
+            continue
+        if start_c>end_c:
+            start_c-=1
+            route_lst.append((start_r,start_c))
+            continue
+
+    return route_lst      
 def solution(points, routes):
-    result = []
-    length = len(routes)
-    robots_place = []
 
-    for i in range(length):
-        robots_place.append(points[routes[i][0] - 1].copy())
-        routes[i].pop(0)
+    answer=0
+    robot=[]
+    for route in routes:
+        route_lst=[]
+        for i in range(len(route)-1):
+            start = points[route[i]-1]
+            end = points[route[i+1]-1]
+            start_r = start[0]
+            start_c = start[1]
+            end_r = end[0]
+            end_c = end[1]
+            if i==0:
+                route_lst.append((start_r,start_c))
+            route_list(start_r,start_c, end_r,end_c,route_lst )
 
-    for i in range(length):
-        temp = [robots_place[i].copy()]
-        while routes[i]:
-            target = points[routes[i][0] - 1]
-            if robots_place[i] == target:
-                routes[i].pop(0)
+        robot.append(route_lst)
+    max_ = max([len(r) for r in robot])    
+    for i in range(max_):
+        lst=[]
+        for r in range(len(routes)):
+            if len(robot[r])<=i:
+                continue
             else:
-                if robots_place[i][0] < target[0]:
-                    robots_place[i][0] += 1
-                elif robots_place[i][0] > target[0]:
-                    robots_place[i][0] -= 1
-                elif robots_place[i][1] < target[1]:
-                    robots_place[i][1] += 1
-                elif robots_place[i][1] > target[1]:
-                    robots_place[i][1] -= 1
+                lst.append(robot[r][i])
 
-                temp.append(robots_place[i].copy())
-        result.append(temp)
+        crash_dash = Counter(lst)
+        for k,v in crash_dash.items():
+            if v>1:
+                answer+=1
 
-    max_length = max(len(subarray) for subarray in result)
-    for i in range(len(result)):
-        gap = max_length - len(result[i])
-        while gap:
-            result[i].append([0, 0])
-            gap -= 1
 
-    transposed = [
-        [item for item in group if item != [0, 0]]
-        for group in zip(*result)
-    ]
-
-    count = 0
-
-    for i in transposed:
-        for j in i[:]:
-            temp = j
-            len_temp = len(i)
-            while j in i:
-                i.remove(temp)
-            if len_temp - len(i) > 1:
-                count += 1
-    return count
+    return answer
