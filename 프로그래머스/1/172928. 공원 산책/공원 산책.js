29f62c24d351c20023d1ca8404f9fe37e94685e4
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-01-18 22:33:40 +0900

    [level 1] Title: 공원 산책, Time: 6.80 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -1,62 +1,67 @@
 function solution(park, routes) {
-
     let pathArr = [];
-
     const WIDTH = park[0].length;
-
     const HEIGHT = park.length;
 
     for(let rowIdx = 0; rowIdx < park.length; rowIdx++){
-
         const row = park[rowIdx].split("");
-
         if(row.includes("S")){
-
             const columnIdx = park[rowIdx].indexOf("S");
-
             pathArr.push([columnIdx, rowIdx]);
-
             break;
-
         }
-
     }
 
     for(let i = 0; i<routes.length; i++) {
-
         const DIRECTION = routes[i].split(' ')[0];
-
         const AMOUNT = Number(routes[i].split(' ')[1]);
-
         let blockPos = [-1, -1];
-
         let x = pathArr[pathArr.length-1][0];
-
         let y = pathArr[pathArr.length-1][1];
-        
-        if (DIRECTION === "E") {
-            if(park[y].slice(x, x+1+AMOUNT).includes("X")) continue;
-            x = x + AMOUNT;
-        } else if (DIRECTION === "W"){
-            if(park[y].slice(x - AMOUNT >= 0 ? x - AMOUNT : 0, x).includes("X")) continue;
-            x = x - AMOUNT;
-        } else if (DIRECTION === "S"){
-            if(park.findIndex((row, idx) => idx <= y + AMOUNT && idx >= y && row[x] === "X") !== -1) continue;
-            y = y + AMOUNT;
-        } else if (DIRECTION === "N"){
 
-            if(park.slice(0, y).findIndex((row, idx) => idx >= y - AMOUNT && row[x] === "X") !== -1) continue;
-            y = y - AMOUNT;
+        if (DIRECTION === "E") {
+            if(park[y].slice(x, x+AMOUNT+1).includes("X")) continue;
+            blockPos = [park[y].indexOf("X", x), y];
+            x = pathArr[pathArr.length-1][0] + AMOUNT;
+            y = pathArr[pathArr.length-1][1];
+        } else if (DIRECTION === "W"){
+            if(park[y].slice(x-AMOUNT >= 0 ? x-AMOUNT : 0, x).includes("X")) continue;
+            blockPos = [park[y].indexOf("X"), y];
+            x = pathArr[pathArr.length-1][0] - AMOUNT;
+            y = pathArr[pathArr.length-1][1];
+        } else if (DIRECTION === "S"){
+            blockPos = [x, park.findIndex((row, idx) => idx >= y && row[x] === "X")];
+            x = pathArr[pathArr.length-1][0];
+            y = pathArr[pathArr.length-1][1] + AMOUNT;
+        } else if (DIRECTION === "N"){
+            blockPos = [x, park.slice(0, y).reverse().findIndex((row, idx) => row[x] === "X")];
+            if (blockPos[1] !== -1) blockPos[1] = y - blockPos[1] - 1; // 역순으로 찾았으므로 인덱스 변환
+            x = pathArr[pathArr.length-1][0];
+            y = pathArr[pathArr.length-1][1] - AMOUNT;
         }
+
+
 
         if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT){
             continue;
         }
+        if(!blockPos.includes(-1)){
+            if((y === blockPos[1] && x >= blockPos[0] && pathArr[pathArr.length > 1 ? pathArr.length-2 : 0][0] < blockPos[0]) || 
+               (y === blockPos[1] && x <= blockPos[0] && pathArr[pathArr.length > 1 ? pathArr.length-2 : 0][0] > blockPos[0]) 
+||
+               (x === blockPos[0] && y >= blockPos[1] && pathArr[pathArr.length > 1 ? pathArr.length-2 : 0][1] < blockPos[1]) 
+||
+                (x === blockPos[0] && y <= blockPos[1] && pathArr[pathArr.length > 1 ? pathArr.length-2 : 0][1] > blockPos[1]) ) {
+                continue;
+            }
+        }
+
+
 
         pathArr.push([x, y]);
-        // console.log([x,y]);
+
     }
 
+    console.log(pathArr);
     return pathArr[pathArr.length-1].reverse();
-
 }
