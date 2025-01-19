06b6c5f56e79228e4be113e9878d56081feae69e
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-01-19 17:56:13 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,3 +1,9 @@
 def solution(common):
-    return  common[-1]+common[-1]-common[-2] if (common[-1]-common[-2] == common[-2]-common[-3]) else common[-1]*common[-1]//common[-2]
-
+    answer = 0
+    d = common[1] - common[0] 
+    if d == (common[2] - common[1]):
+        answer = common[-1] + d
+    else:
+        r = common[1] / common[0]
+        answer = common[-1] * r
+    return answer
