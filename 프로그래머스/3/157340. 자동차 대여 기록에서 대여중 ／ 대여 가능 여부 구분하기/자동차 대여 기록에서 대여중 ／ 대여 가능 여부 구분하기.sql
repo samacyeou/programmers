3e968b5237a136b7f5465e3787f49001861cb45c
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-02-15 07:48:34 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,30 +1,36 @@
 -- 코드를 입력하세요
-SELECT
-    O.CAR_ID AS CAR_ID,
-    IF(P.CAR_ID IS NULL, '대여 가능', '대여중') AS AVAILABILITY
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY AS O
-    LEFT JOIN (
-        SELECT CAR_ID
+# SELECT
+#     O.CAR_ID AS CAR_ID,
+#     IF(P.CAR_ID IS NULL, '대여 가능', '대여중') AS AVAILABILITY
+# FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY AS O
+#     LEFT JOIN (
+#         SELECT CAR_ID
+#         FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+#         WHERE
+#             START_DATE <= '2022-10-16' AND
+#             END_DATE >= '2022-10-16'
+#     ) AS P
+#     ON O.CAR_ID = P.CAR_Id
+# GROUP BY O.CAR_ID
+# ORDER BY O.CAR_ID DESC
+
+
+WITH B AS (
+    SELECT DISTINCT CAR_ID
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    WHERE START_DATE <= '2022-10-16' AND END_DATE >= '2022-10-16'
+)
+
+SELECT A.CAR_ID,
+       CASE 
+           WHEN B.CAR_ID IS NULL THEN '대여 가능'
+           ELSE '대여중'
+       END AS "AVAILABILITY"
+FROM (
+        SELECT DISTINCT CAR_ID
         FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-        WHERE
-            START_DATE <= '2022-10-16' AND
-            END_DATE >= '2022-10-16'
-    ) AS P
-    ON O.CAR_ID = P.CAR_Id
-GROUP BY O.CAR_ID
-ORDER BY O.CAR_ID DESC
-
-
-# SELECT CAR_ID, 
-#        CASE WHEN MAX(START_DATE) < '2022-10-16' AND '2022-10-16' < MAX(END_DATE) THEN '대여중'
-#             WHEN '2022-10-16' = MAX(END_DATE) THEN '대여중'
-#             ELSE '대여 가능'
-#             END AS AVAILABILITY
-# FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-# GROUP BY 1
-# ORDER BY 1 DESC;
-
-
-# select *
-# from car_rental_company_rental_history
-# order by car_id desc
+        GROUP BY CAR_ID
+     ) A
+LEFT JOIN B
+ON A.CAR_ID = B.CAR_ID
+ORDER BY A.CAR_ID DESC;
