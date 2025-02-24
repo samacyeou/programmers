2da258ea3cb07f3f8d70e481cbb54e87a07ed000
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-02-24 22:21:42 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 49.96 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712?language=java) 
 
 ### 성능 요약
 
-메모리: 39 MB, 시간: 17.10 ms
+메모리: 94.4 MB, 시간: 49.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 09일 15:02:35
+2025년 02월 24일 22:21:41
 
 ### 문제 설명
 
--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,86 +1,54 @@
-import java.util.*;
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        Map<String,Integer> result = new HashMap<>();
-        Map<String, ArrayList<Object>> map = new HashMap<String, ArrayList<Object>>();
-        Map<String, Set<Object>> yn = new HashMap<String, Set<Object>>();
-        Map<String,Integer> giftScore = new HashMap<>();
-        for (String value : friends) {
-            map.put(value, new ArrayList<>()); 
-            yn.put(value, new HashSet<>()); 
-            giftScore.put(value,0);
-            result.put(value,0);
-        }
+        int[] givePresent, receivePresent, presentRate, willReceive;
+        int[][] presentRelate = new int[friends.length][friends.length];   // 선물 준 사람과 선물 받은 사람의 관계도
+        givePresent = new int[friends.length];
+        receivePresent = new int[friends.length];
+            presentRate = new int[friends.length];
+        willReceive = new int[friends.length];
+        int answer = 0;
 
-        for(String value : gifts){
-            String[] gift = value.split(" ");
-            map.get(gift[0]).add(gift[1]);
-            giftScore.put(gift[0], giftScore.get(gift[0])+1);
-            giftScore.put(gift[1], giftScore.get(gift[1])-1);
-            yn.get(gift[0]).add(gift[1]);
-            yn.get(gift[1]).add(gift[0]);
-        }
+        for(int i = 0; i < gifts.length; i++){
+            String[] tmp = gifts[i].split(" ");   // 매개변수로 준 gifts 배열을 공백을 기준으로 준 사람, 받은 사람으로 나눔.
 
-        for(String friend : friends){
-            if(yn.get(friend).size() == friends.length-1){
-                Set<Object> target = yn.get(friend);
-                for(Object value : target){
-                    int temp = 0; 
-                    int temp2 = 0; 
-                    for(int i=0; i<map.get(value).size(); i++){
-                        if(map.get(value).get(i).equals(friend)) temp++;
-                    } 
-                    for(int i=0; i<map.get(friend).size(); i++){
-                        if(map.get(friend).get(i).equals(value)) temp2++;
-                    }
-                    if(temp<temp2){
-                        result.put(friend,result.get(friend)+1);
-                    }
-                    else if(temp == temp2 && (giftScore.get(value) < giftScore.get(friend))){
-                        result.put(friend,result.get(friend)+1);
-                    }
-                }
-            }
-            else{ 
-                Set<Object> target = yn.get(friend);
-                for(String v : friends){
-                    if( !target.contains(v) && !v.equals(friend)){
-                        String noGift = v;
-                        if(giftScore.get(friend) > giftScore.get(noGift)  ){
-                            result.put(friend,result.get(friend)+1);
+            for(int j = 0; j < friends.length; j++){   // for문을 돌려서 매개변수로 준 friends 배열을 기준으로 하여 통계를 채널링함.
+                if(friends[j].equals(tmp[0])){
+                    givePresent[j]++;
+
+                    for(int k = 0; k < friends.length; k++){
+                        if(tmp[1].equals(friends[k])){
+                            presentRelate[j][k]++;
+                            receivePresent[k]++;
                         }
                     }
                 }
-                for(Object value : target){
-                    int temp = 0; 
-                    int temp2 = 0; 
-                    for(int i=0; i<map.get(value).size(); i++){
-                        if(map.get(value).get(i).equals(friend))
-                            temp++;
-                    } 
-                    for(int i=0; i<map.get(friend).size(); i++){
-                        if(map.get(friend).get(i).equals(value))
-                            temp2++;
-                    }
-                    if(temp<temp2){ 
-                        result.put(friend,result.get(friend)+1);
-                    }
-                    else if(temp == temp2 && (giftScore.get(value) != giftScore.get(friend))){
-                        result.put(friend,result.get(friend)+1);
-                    }
+            }
+        }
+        
+        for(int i = 0; i < friends.length; i++) System.out.println(givePresent[i]+" "+receivePresent[i]+" "+presentRate[i]);
+
+        for(int i2 = 0; i2 < friends.length; i2++) presentRate[i2] = givePresent[i2] - receivePresent[i2];   // 선물지수
+
+        
+        for(int index = 0; index < friends.length; index++){   // 서로 주고받은 선물 개수 비교
+            for(int r = 0; r < friends.length; r++){
+                if(index == r) continue;
+                if(presentRelate[index][r] > presentRelate[r][index]){
+                    willReceive[index]++;
+                }
+                else if(presentRelate[index][r] == presentRelate[r][index]){
+                    if(presentRate[index] > presentRate[r]) willReceive[index]++;
+                    else if(presentRate[index] == presentRate[r]) continue;
                 }
             }
         }
-        return getMaxValue(result);
-    }
 
-    public static int getMaxValue(Map<String, Integer> map) {
-        int max = Integer.MIN_VALUE;
-        for (int value : map.values()) {
-            if (value > max) {
-                max = value;
+        for(int l = 0; l < friends.length; l++){
+            if(willReceive[l] > answer){
+                answer = willReceive[l];
             }
         }
-        return max;
+
+        return answer;
     }
 }
