054023f230fd9db767c90e28122615b49859c768
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-05-07 11:55:01 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 2.49 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.c
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.c
@@ -1,37 +1,20 @@
 #include <stdio.h>
-#define LEN_INPUT 10
+#define LEN_INPUT 20
 
-int main(void)
-{
+int main(void) {
     char s1[LEN_INPUT];
     scanf("%s", s1);
 
-size_t len1 = strlen(s1);
-
-//배열을 입력받음
-//입력받은 배열이 소문자이면 + 32
-//입력받은 배열이 대문자이면 -32
-
-char s2[len1];
-
-for (int i =0; i<len1; i++)
-{
-    if(s1[i] >='A' && s1[i] <= 'Z')
-    {
-    s2[i] = s1[i]+32;
-
+    for (int i=0; s1[i]!='\0'; i++) {
+        if (s1[i] >= 'a' && s1[i] <= 'z') {
+            s1[i] = s1[i] - 32;
+        }
+        else if (s1[i] >= 'A' && s1[i] <= 'Z') {
+            s1[i] = s1[i] + 32;
+        }
     }
-    else if( s1[i] >= 'a' && s1[i] <= 'z')
-    {
-    s2[i] = s1[i]-32;  
-    }
-    else
-    {
-    s2[i] = s1[i];
-    }
-}
-    s2[len1] = '\0';  
-    printf("%s\n", s2);
 
+    printf("%s", s1);
     return 0;
+
 }
