5661c70e43e18fc35eeb59abea815533d56c5127
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-05-09 16:19:18 +0900

    [level 2] Title: 자동차 평균 대여 기간 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/157342. 자동차 평균 대여 기간 구하기/README.md
+++ 프로그래머스/2/157342. 자동차 평균 대여 기간 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 자동차 평균 대여 기간 구하기 - 157342 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/157342#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/157342?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 12월 09일 15:07:25
+2025년 05월 09일 16:19:18
 
 ### 문제 설명
 
--- 프로그래머스/2/157342. 자동차 평균 대여 기간 구하기/자동차 평균 대여 기간 구하기.sql
+++ 프로그래머스/2/157342. 자동차 평균 대여 기간 구하기/자동차 평균 대여 기간 구하기.sql
@@ -1,5 +1,21 @@
-select car_id, to_char(round(avg(end_date - start_date + 1), 1), 'FM9990.0') as average_duration
-from car_rental_company_rental_history
-group by car_id 
-having avg(end_date-start_date+1) >= 7
-order by to_number(average_duration) desc, car_id desc
+-- select car_id, to_char(round(avg(end_date - start_date + 1), 1), 'FM9990.0') as average_duration
+-- from car_rental_company_rental_history
+-- group by car_id 
+-- having avg(end_date-start_date+1) >= 7
+-- order by to_number(average_duration) desc, car_id desc
+
+
+select A.car_id
+     , to_char(round(avg(A.D_PER_CPTA), 1),'FM9990.0') as "AVERAGE_DURATION"
+  from (select HISTORY_ID
+             , CAR_ID
+             , TO_CHAR(START_DATE, 'YYYY-MM-DD')
+             , TO_CHAR(END_DATE, 'YYYY-MM-DD')
+             , case when END_DATE - START_DATE + 1 > 0
+                    then END_DATE - START_DATE + 1
+                    else 0
+                     end as "D_PER_CPTA" -- 건당 대여일수  
+          from CAR_RENTAL_COMPANY_RENTAL_HISTORY) A
+ group by A.car_id
+having avg(A.D_PER_CPTA) >= 7
+ order by to_number(average_duration) desc, 1 desc
