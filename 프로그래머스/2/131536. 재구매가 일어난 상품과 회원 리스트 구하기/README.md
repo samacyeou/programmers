750b7fe585951f641d6849bb5af2c79ac068dec5
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-05-09 16:26:45 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/README.md
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 재구매가 일어난 상품과 회원 리스트 구하기 - 131536 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131536#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131536?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 10월 18일 10:58:59
+2025년 05월 09일 16:26:46
 
 ### 문제 설명
 
--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -1,6 +1,19 @@
--- 코드를 입력하세요
-SELECT USER_ID, PRODUCT_ID
-FROM ONLINE_SALE
-GROUP BY USER_ID, PRODUCT_ID
-HAVING COUNT(PRODUCT_ID) > 1
-ORDER BY USER_ID ASC, PRODUCT_ID DESC
+select distinct C.USER_ID, C.PRODUCT_ID
+  from (select B.USER_ID
+             , B.PRODUCT_ID
+          from (select SME_USR_PRDT
+                     , count(SME_USR_PRDT)
+                  from (Select ONLINE_SALE_ID
+                             , USER_ID
+                             , PRODUCT_ID
+                             , SALES_AMOUNT
+                             , SALES_DATE
+                          ,    USER_ID || '_' || PRODUCT_ID as "SME_USR_PRDT"
+                          from ONLINE_SALE ) 
+                 group by SME_USR_PRDT
+                having count(SME_USR_PRDT) >= 2) A
+     left join (select *
+                  from ONLINE_SALE) B
+       on A.SME_USR_PRDT = B.USER_ID || '_' || B.PRODUCT_ID) C
+       order by 1, 2 desc
+  ;
