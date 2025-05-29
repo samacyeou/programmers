ea0feb9a0f3ecbf56099d54e936659ef1a9d59e8
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-05-29 12:11:47 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 5.38 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/README.md
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 대소문자 바꿔서 출력하기 - 181949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181949#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181949?language=c) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 2.49 ms
+메모리: 4.13 MB, 시간: 5.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 07일 11:54:58
+2025년 05월 29일 12:11:47
 
 ### 문제 설명
 
--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.c
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include<ctype.h>
 #define LEN_INPUT 20
 
 int main(void) {
     char s1[LEN_INPUT];
     scanf("%s", s1);
 
-    for (int i=0; s1[i]!='\0'; i++) {
-        if (s1[i] >= 'a' && s1[i] <= 'z') {
-            s1[i] = s1[i] - 32;
-        }
-        else if (s1[i] >= 'A' && s1[i] <= 'Z') {
-            s1[i] = s1[i] + 32;
-        }
+    for(int i=0; i<LEN_INPUT; i++){
+        if(isupper(s1[i])){ 
+            s1[i] = tolower(s1[i]); //소문자 -> 대문자
+        } 
+        else
+        { 
+            s1[i] = toupper(s1[i]); //대문자 -> 소문자
+        } 
     }
+    printf("%s",s1);
 
-    printf("%s", s1);
     return 0;
-
 }
