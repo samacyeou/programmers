28256ad6a49bb710ec1790c3334a3228aa7dfba5
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-06-10 19:48:36 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/README.md
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 5] 상품을 구매한 회원 비율 구하기 - 131534 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131534#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131534?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2025년 03월 31일 07:33:40
+2025년 06월 10일 19:48:36
 
 ### 문제 설명
 
--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,42 +1,16 @@
-/*WITH USER2021 AS(
-    SELECT *
-    FROM USER_INFO
-    WHERE TO_CHAR(JOINED,'YYYY') ='2021'
-),
-SALES2022 AS(
-    SELECT *
-    FROM ONLINE_SALE
-    WHERE TO_CHAR(SALES_DATE,'YYYY') ='2022'
-)
 SELECT
-    YEAR,
-    MONTH,
-    COUNT(DISTINCT USER_ID) AS PURCHASED_USERS,
-    to_number(to_char(ROUND(
-        COUNT(DISTINCT USER_ID) /
-        (
-            SELECT COUNT(USER_ID)
-            FROM USER2021
-        ), 1
-    ), 'FM9990.0')) AS PUCHASED_RATIO
-FROM
-    (
-        SELECT
-            EXTRACT(YEAR FROM SALES_DATE) YEAR,
-            EXTRACT(MONTH FROM SALES_DATE) MONTH,
-            U.USER_ID
-        FROM SALES2022 O , USER2021 U
-        WHERE O.USER_ID = U.USER_ID
-    )
-GROUP BY YEAR , MONTH
-ORDER BY YEAR, MONTH;*/
+    YEAR(ONLINE_SALE.SALES_DATE) AS YEAR,
+    MONTH(ONLINE_SALE.SALES_DATE) AS MONTH,
+    COUNT(DISTINCT ONLINE_SALE.USER_ID) AS PUCHASED_USERS,
+    ROUND(
+        COUNT(DISTINCT ONLINE_SALE.USER_ID) /
+        (SELECT COUNT(USER_ID)
+        FROM USER_INFO
+        WHERE YEAR(JOINED) = '2021'),1
+        ) AS PUCHASED_RATIO
+FROM ONLINE_SALE JOIN USER_INFO
+    ON ONLINE_SALE.USER_ID = USER_INFO.USER_ID
+WHERE YEAR(USER_INFO.JOINED) = '2021'
+GROUP BY YEAR(ONLINE_SALE.SALES_DATE), MONTH(ONLINE_SALE.SALES_DATE)
+ORDER BY YEAR(ONLINE_SALE.SALES_DATE), MONTH(ONLINE_SALE.SALES_DATE)
 
-
-
-select to_char(o.sales_date,'YYYY') YEAR, to_char(o.sales_date,'FMMM') MONTH,
-    count(distinct o.user_id) PURCHASED_USERS,
-    to_number(to_char(round(count(distinct o.user_id)/(select count(user_id) from user_info where extract(year from joined) = '2021'), 1),'FM9990.0')) PUCHASED_RATIO
-from user_info u join online_sale o on u.user_id = o.user_id
-where extract(year from u.joined) = '2021'
-group by to_char(o.sales_date,'YYYY'), to_char(o.sales_date,'FMMM')
-order by to_number(YEAR), to_number(MONTh)
