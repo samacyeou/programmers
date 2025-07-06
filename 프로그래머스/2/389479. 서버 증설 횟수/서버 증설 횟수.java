19f4ecff07c72ce798f408de9b580abe4d740b63
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-07-06 16:24:26 +0900

    [level 2] Title: 서버 증설 횟수, Time: 4.18 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -8,27 +8,30 @@ class Solution {
         List<Integer> timer = new ArrayList<>();
 
         for (int i = 0; i < players.length; i++) {
-//             for (int j = 0; j < timer.size(); j++) {
-//                 timer.set(j, timer.get(j) - 1);
-//             }
+            for (int j = 0; j < timer.size(); j++) {
+                timer.set(j, timer.get(j) - 1);
+            }
 
-//             for (int j = timer.size() - 1; j >= 0; j--) {
-//                 if (timer.get(j) == 0) {
-//                     timer.remove(j);
-//                 }
-//             }
+            for (int j = timer.size() - 1; j >= 0; j--) {
+                if (timer.get(j) == 0) {
+                    timer.remove(j);
+                    server--;
+                }
+            }
             
-            if(time.containsKey(i)) server -= time.get(i);
+            // if(time.containsKey(i)) server -= time.get(i);
             
-            if (players[i] >= (server + 1) * m) {
+            if (players[i] >= (timer.size() + 1) * m) {
                 int n = players[i] / m;
-                answer += n - server;
-                time.put(i + k, n - server);
-                server += n - server;
+                int add = n - timer.size();
+                answer += add;
+                time.put(i + k, add);
+                // server += add;
 
-                // for (int j = 0; j < n - server; j++) {
-                //     timer.add(k);
-                // }
+                for (int j = 0; j < add; j++) {
+                    timer.add(k);
+                    server++;
+                }
             }
         }
 
