97de8bab982814c8f57256366ae90610b61d7c24
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-07-10 06:18:36 +0900

    [level 2] Title: 단체사진 찍기, Time: 332.45 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+bool check(string s, vector<char>* condition, int n){
+    for(int i=0;i<n;i++){
+        int first_idx = s.find(condition[i][0]); //N
+        int second_idx = s.find(condition[i][1]); //F
+        char cond = condition[i][2]; //=
+        int num = condition[i][3]-'0'; // 2
+        int dist = abs(first_idx - second_idx);
+        if(cond == '>'){
+            if(!(dist-1>num)) return false;
+        }else if(cond == '<'){
+            if(!(dist-1<num)) return false;
+        }else{
+            if(dist-1 != num) return false;
+        }
+    }
+    return true;
+}
+
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+int solution(int n, vector<string> d) {
+    int answer = 0;
+    string s = "ACFJMNRT";
+    vector<char> condition[n];
+    
+    for(int i=0;i<n;i++){
+        condition[i].push_back(d[i][0]);
+        condition[i].push_back(d[i][2]);
+        condition[i].push_back(d[i][3]);
+        condition[i].push_back(d[i][4]);
+    }
+
+    do{
+        if(check(s, condition, n)) answer++;
+    }while(next_permutation(s.begin(),s.end()));
+
+    return answer;
+}
