164c8aded4da26ad74d7ce7a2ced7f9f9ff7d0e6
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-07-16 12:24:24 +0900

    [level 1] Title: 소수 찾기, Time: 1915.27 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,20 +1,31 @@
-def divisor(num):
-    temp = 0
-    for i in range(2, int(num**(1/2))+1):
-        if num % i == 0:
-            #temp.append(i)
-            temp += 2
-            break
-    # print(temp)
-    return temp
+import math
 
+def isPrime(number,myList):
+    if(number==3):
+        return True
+
+    for num in myList:
+        if(num>math.sqrt(number)):
+            return True
+        if(number%num==0):
+            return False
+    return True
 
 def solution(n):
+
+    primeList=[2]
+
     answer = 0
-    for i in range(2, n+1):
-        temp = divisor(i)
-        if temp != 2:
-            #answer.append(i) 
-            answer += 1 
+    if(n==2):
+        return 1
+    else:
+        answer+=1
+        for num in range(3,n+1,2):
+            #근데 소수의 배수인지 확인하려면, 이걸 또 순회해야하는거 아닌가
+            #일단 이건 어쩔수없는 순회임(숫자 한번씩 확인해야하니)
+            #이걸 홀수만 확인해서, 갯수줄임
+            if(isPrime(num,primeList)==True):
+                primeList.append(num)
+                answer+=1
 
     return answer
