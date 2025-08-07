51b5fe798f5f765bc074a471ba914a1029893610
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-08-07 13:51:28 +0900

    [level 2] Title: 2 x n 타일링, Time: 3.25 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -1,10 +1,8 @@
 function solution(n) {
-let a = 1;
-let b = 2;
-for (let i = 3; i <= n; i++) {
-    var next = (a + b + 1000000007) % 1000000007
-    a = b;
-    b = next;
-}
-return b;
+    const dp = [0,1,2]
+    for(let i=3; i<=n; i++) {
+        dp[i] = (dp[i-2] + dp[i-1]) % 1_000_000_007;
+
+    }
+    return dp[n];
 }
