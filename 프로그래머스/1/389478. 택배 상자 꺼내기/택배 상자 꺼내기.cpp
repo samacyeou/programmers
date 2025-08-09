bfd4f08325a6ce0da9406b6c60238df6c39dafd9
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-08-09 19:04:28 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
@@ -1,40 +1,30 @@
-#include <string>
-#include <vector>
-
+#include <iostream>
 using namespace std;
 
+// num = 상자번호, w = 한 층 최대 상자 수, n = 전체 상자 수
 int solution(int n, int w, int num) {
-    int answer = 0; // 타겟박스값 0으로 값 초기화
-int column = 0; // 높이값 0으로 값 초기화
+    int answer = 0;
 
-int h = (n + w - 1) / w;  // * 높이설정 -> 인트값이라 정수부분만 표시됨* -> 왜 w-1을 우겨넣어 높이 식을 만든건지??
+    int floor = (num + w - 1) / w;       // 현재 층 (1부터)
+    int locate = (num - 1) % w;          // 해당 층 내 위치
+    // 홀수층 짝수층 번호 맞추기
+    int col = (floor % 2 == 1) ? locate : (w - 1 - locate);
 
-vector<vector<int>> v(h, vector<int>(w, -1)); // 2차원 벡터 설정 -> { {h, vector<int>(w, -1)} {h, vector<int>(w, -1)} } 이런 식으로
+    int maxfloor = (n + w - 1) / w;      // 총 층 수
 
-int boxNumber =1; // 갱신되는 박스값 초기화
-
-for(int r = 0; r <h; r++){ // 
-    if(r % 2 == 0){ // 지그재그로 쌓기 위해서
-        for(int c = 0; c < w && boxNumber <= n; c++){
-            if(boxNumber == num){
-                column = c;
-            }
-            v[r][c] = boxNumber++;
+    for (int i = floor; i <= maxfloor; ++i) {
+        int BoxfloorCount; // 마지막 층 상자 개수
+        if (i == maxfloor) {
+            BoxfloorCount = n - (i - 1) * w;
+        } else {
+            BoxfloorCount = w;
         }
-    }else{
-        for(int c = w-1; c>=0 && boxNumber <= n; c--){
-            if(boxNumber == num){
-                column = c;
-            }
-            v[r][c] = boxNumber++;
+
+        if (i % 2 == 1) {
+            if (col <= BoxfloorCount) answer++;
+        } else {
+            if (col >= w - BoxfloorCount) answer++;
         }
     }
-}
-
-for(int r = 0; r<h; r++){
-    if(v[r][column] < num) continue;
-    else answer++;
-}
-
-return answer;
+    return answer;
 }
