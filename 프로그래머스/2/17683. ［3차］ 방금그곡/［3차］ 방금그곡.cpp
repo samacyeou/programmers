3147ad17c0f8dd45f41a07c6211a5d7fd95e648f
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-10-02 04:07:09 +0900

    [level 2] Title: [3차] 방금그곡, Time: 0.53 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.cpp
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.cpp
@@ -1,49 +1,55 @@
 #include <bits/stdc++.h>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-string convHash(string music) {
-    music += '0';
-    string s;
-    for(int i = 0; i < music.length() - 1; i++) {
-        if(music[i+1] == '#') {
-            s += (char)(music[i] + 8);
-            i++;
-        } else s += music[i];
+void convHash(string &music) {
+    map<char, char> m = {{'C', 'Q'}, {'D', 'R'}, {'F', 'S'},
+                         {'G', 'T'}, {'A', 'U'}};
+
+    for(int i = 0; i < music.length(); i++) {
+        if (music[i] == '#') { 
+            music[i - 1] = m[music[i - 1]];
+            music.erase(music.begin() + i);
+            i--;
+        }
     }
-    
-    return s;
 }
 
-string solution(string s, vector<string> musicinfos) {
+string solution(string m, vector<string> musicinfos) {
     string answer = "(None)";
     int ansTime = 0;
-    string m = convHash(s);
+    convHash(m);
 
     for (int i = 0; i < musicinfos.size(); i++) {
         string t1;
         string t2;
         string name;
-        string mus;
+        string music;
         string info = musicinfos[i];
 
-//         replace(info.begin(),info.end(), ',', ' ');
+        // replace(info.begin(),info.end(), ',', ' ');
+        
+        // string temp = " ";
+        // while(info.find(',') != string::npos) {
+        //     info.replace(info.find(','), 1, temp);
+        // }
 
-//         stringstream ss(info);
-//         ss >> t1 >> t2 >> name >> mus;
+        // stringstream ss(info);
+        // ss >> t1 >> t2 >> name >> mus;
         
         t1 = info.substr(0, 5);
         t2 = info.substr(6, 5);
         int idx = info.find(',', 12);
         name = info.substr(12, idx-12);
-        mus = info.substr(idx+1);
+        music = info.substr(idx+1);
 
         int start = stoi(t1.substr(0, 2)) * 60 + stoi(t1.substr(3));
         int end = stoi(t2.substr(0, 2)) * 60 + stoi(t2.substr(3));
         int sub = end - start; 
 
-        string music = convHash(mus);
+        convHash(music);
         string played = "";
         int len = music.length();
 
