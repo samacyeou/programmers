66ffea3288c71cc1012d9a7cd83c617adffcb150
samacyeou <151595973+samacyeou@users.noreply.github.com>
2025-12-26 16:26:14 +0900

    [level 2] Title: 양궁대회, Time: 4315.35 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/README.md
+++ 프로그래머스/2/92342. 양궁대회/README.md
@@ -1,10 +1,10 @@
 # [level 2] 양궁대회 - 92342 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342#qna) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 2.78 ms
+메모리: 9.24 MB, 시간: 4315.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 13일 09:19:49
+2025년 12월 26일 16:26:11
 
 ### 문제 설명
 
--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -1,98 +1,70 @@
-def dfs(info, n, index, cur, curList):
-    if n == 0 or index == 10:
-        cur[10] = n
+def tree(n, appeach_info, ryan_info, tree_node):
 
-        apeachP = 0
-        lionP = 0
-        arrowP = 0
-        temp = [0] * 13
+    if n < 0:
+        return -1
 
-        for i in range(11):
-            if info[i] != 0 and info[i] >= cur[i]: apeachP += 10 - i
-            elif cur[i] > info[i]: lionP += 10 -i
-
-            temp[i] = cur[i]
-            arrowP += cur[i] * (10 - i)
-
-        if(lionP > apeachP):
-            temp[11] = lionP - apeachP
-            temp[12] = arrowP
-            curList.append(temp)
-
-        return
-
-    if n > info[index]:
-        cur[index] = info[index] + 1
-        dfs(info, n - (info[index] + 1), index + 1, cur, curList)
-
-    cur[index] = 0
-    dfs(info, n, index + 1, cur, curList)
-
-    return
-    
-def solu(n, info):
-    answer = []
-    result = [0] * 11
-    temp = [0] * 13
-    maxDiff = 0
-    minArrowP = 0
-    idx = 0
-    win = []
-
-    dfs(info, n, 0, result, win)
-
-    if(len(win) == 0): answer = [-1]
-    else:
-        for i in range(len(win)):
-            temp = win[i]
-
-            if temp[11] > maxDiff or (temp[11] == maxDiff and temp[12] < minArrowP):
-                maxDiff = temp[11]
-                minArrowP = temp[12]
-                idx = i
-
-        answer = win[idx][0:11]
-
-    if n>10: return [-2]
-    return answer
-    
-def solution(n, info):
-    counterpart_score = sum((10-i) for i in range(10) if info[i])
-    # 어피치의 점수의 총합
-    dp = [[0,[]] for _ in range(n+1)]
-    # 총 점수, 점수를 얻은 칸 위치 리스트 n은 들어간 화살 갯수
-    for i in range(10):
-        c = info[i]
-        # 상대방이 맞춘 화살이 있다면
-        if c != 0:
-            for j in range(n-c):
-                # 내가 점수를 얻어서 상대방의 점수가 줄어든 경우 (10-i) * 2 추가
-                if (j==0 or dp[j][0]) and dp[j][0]+2*(10-i)>= dp[j+c+1][0] and 10-i not in dp[j][1]:
-                    dp[j+c+1][0] = dp[j][0]+2*(10-i)
-                    dp[j+c+1][1]=dp[j][1].copy()
-                    dp[j+c+1][1].append(10-i)
+    if n == 0:
+        appeach_score, ryan_score = 0, 0
+        for i in range(len(appeach_info)):
+            if appeach_info[i] < ryan_info[i] :
+                ryan_score += (10 - i)
+            else:
+                if appeach_info[i] != 0:
+                    appeach_score += (10 - i)
+        if ryan_score > appeach_score:
+            # print(f"{ryan_score} : {ryan_info} / {appeach_score}")
+            return (ryan_score - appeach_score, ryan_info[:])
         else:
-            # 상대방이 맞춘 화살이 없어서 하나만 투자해도 점수를 얻을 수 있는 경우 (10-i) 추가
-            for j in range(n):
-                if (j==0 or dp[j][0]) and dp[j][0]+(10-i) >= dp[j+1][0] and 10-i not in dp[j][1]:
-                    dp[j+1][0] = dp[j][0]+(10-i)
-                    dp[j+1][1]=dp[j][1].copy()
-                    dp[j+1][1].append(10-i)
-        
-    # dp = [[0,[]],[6,[6]],[18,[9]],[24,[9,6]],[34,[9,8]],[40,[9,8,6]]]
-    max_comb = max(dp, key= lambda x:x[0])
-    if max_comb[0] <= counterpart_score:
+            return -1
+
+    else:
+        ans = []
+        for i in range(len(appeach_info)):
+            if tree_node[i]:
+                tree_node[i] = False
+                ryan_info[i] += (appeach_info[i] + 1)
+                n -= (appeach_info[i] + 1)
+
+                a = tree(n, appeach_info, ryan_info, tree_node)
+
+                tree_node[i] = True
+                ryan_info[i] -= (appeach_info[i] + 1)
+                n += (appeach_info[i] + 1)
+
+                if a != -1:
+                    ans.append(a)
+
+        # 모두 0점에 꽂히는 케이스 추가
+        a = ryan_info[:]
+        a[-1] += n
+        appeach_score, ryan_score = 0, 0
+        for i in range(len(appeach_info)):
+            if appeach_info[i] < a[i] :
+                ryan_score += (10 - i)
+            else:
+                if appeach_info[i] != 0:
+                    appeach_score += (10 - i)
+        if ryan_score > appeach_score:
+            ans.append((ryan_score - appeach_score, a))
+
+        ans.sort(key= lambda x : (x[0], tuple(reversed(x[1]))))
+        if ans:
+            return ans[-1]
+        else:
+            return -1
+
+
+
+def solution(n, appeach_info):
+    # 변수 설정
+    ryan_info = [0 for _ in range(len(appeach_info))]
+    tree_node = [True for _ in range(len(appeach_info))]
+
+    # 트리 진입
+    a = tree(n, appeach_info, ryan_info, tree_node)
+
+    # 결과 도출
+    if a == -1:
         return [-1]
-
-    temp = [[1,0]]
-    ans = [0]*11
-    s = 0
-    for num in max_comb[1]:
-        ans[10-num] = info[10-num]+1
-        s += info[10-num]+1
-
-    ans[10] = n - s
-
-    test = solu(n, info)
-    
-    return test
+    else:
+        return a[1]
