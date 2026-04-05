28bd36dd3eb3daabb7610f2e4979d01ad3552cdd
samacyeou <151595973+samacyeou@users.noreply.github.com>
2026-04-05 12:26:32 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,36 +1,20 @@
 class Solution {
-    public long solution(long num) {
-        long count = 0;
-        boolean run = true;
+    public int solution(int num) {
+        int answer = 0;
 
-        if(num == 1){
-            count = 0;
-            run = false;
-        }else{
-            while(run)
-            {
-                if(num % 2 == 0){
-                    // 짝수인 경우
-                    count+=1;
-                    num = num/2;
+        if (num == 1) return 0;
 
-                }else if(num == 1){
-                    // 반복문 종료. 결과값 도출
-                    run = false;
-
-                }else{
-                    count+=1;
-                    num = (num * 3) + 1;
-                }
-                
-                if(count > 500){
-                    // 반복문 종료. 카운트 초과
-                    count = -1;
-                    run = false;
-
-                }
+        while (num != 1) {
+            if (answer > 400) return -1;
+            if (num % 2 == 0) {
+                num /= 2;
+                answer++;
+            } else {
+                num *= 3;
+                num++;
+                answer++;
             }
         }
-        return count;
+        return answer;
     }
 }
