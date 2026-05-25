f980215635f29110abd2937c7a1fc3d7a74c903e
samacyeou <151595973+samacyeou@users.noreply.github.com>
2026-05-26 07:30:18 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,47 +1,50 @@
--- 코드를 입력하세요
-# WITH RM AS (
-#     SELECT MEMBER_ID, COUNT(MEMBER_ID) AS COUNT
+# WITH ReviewCounts AS (
+#     SELECT MEMBER_ID, COUNT(*) AS review_count
 #     FROM REST_REVIEW
 #     GROUP BY MEMBER_ID
-#     ORDER BY COUNT DESC
-#     LIMIT 1
+# ),
+# TopReviewers AS (
+#     SELECT MEMBER_ID
+#     FROM ReviewCounts
+#     WHERE review_count = (SELECT MAX(review_count) FROM ReviewCounts)
 # )
 
-# SELECT
-#     M.MEMBER_NAME AS MEMBER_NAME,
-#     R.REVIEW_TEXT AS REVIEW_TEXT,
-#     DATE_FORMAT(R.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
-# FROM MEMBER_PROFILE AS M
-#     JOIN REST_REVIEW AS R
-#         ON M.MEMBER_ID = R.MEMBER_ID
-#     JOIN RM
-#         ON R.MEMBER_ID = RM.MEMBER_ID
-# ORDER BY REVIEW_DATE ASC, REVIEW_TEXT ASC
+# SELECT 
+#     MEMBER_NAME,
+#     REVIEW_TEXT,
+#     DATE_FORMAT(REVIEW_DATE, '%Y-%m-%d')
+# FROM 
+#     REST_REVIEW
+#         JOIN 
+#             TopReviewers ON rest_review.MEMBER_ID = topreviewers.MEMBER_ID
+#         JOIN 
+#             MEMBER_PROFILE ON rest_review.MEMBER_ID = member_profile.MEMBER_ID
+# ORDER BY 
+#     REVIEW_DATE, REVIEW_TEXT;
 
+    
+    
+    
+WITH JOIN_COUNT AS(
+    SELECT
+        r.MEMBER_ID,
+        m.MEMBER_NAME,
+        r.REVIEW_TEXT,
+        r.REVIEW_DATE,
+        COUNT(r.MEMBER_ID) OVER (PARTITION BY r.MEMBER_ID) AS REVIEW_COUNT
+    FROM REST_REVIEW r
+        JOIN MEMBER_PROFILE m
+            ON r.MEMBER_ID = m.MEMBER_ID
+    order by review_count desc
+)
 
-
-
-SELECT 
-       A.MEMBER_NAME, 
-       B.REVIEW_TEXT,
-       DATE_FORMAT(B.REVIEW_DATE,'%Y-%m-%d') AS REVIEW_DATE
-  FROM MEMBER_PROFILE A
-  INNER JOIN REST_REVIEW B
-     ON A.MEMBER_ID = B.MEMBER_ID
-     WHERE A.MEMBER_ID = (SELECT MEMBER_ID
-                         FROM REST_REVIEW 
-                        GROUP BY MEMBER_ID
-                        ORDER BY COUNT(*) DESC
-                        LIMIT 1)
-  ORDER BY REVIEW_DATE, REVIEW_TEXT
-
-
-
-# SELECT *
-# FROM MEMBER_PROFILE
-# JOIN
-# SELECT MEMBER_ID
-#                          FROM REST_REVIEW 
-#                         GROUP BY MEMBER_ID
-#                         ORDER BY COUNT(*) DESC
-#                         LIMIT 1
+SELECT
+    MEMBER_NAME,
+    REVIEW_TEXT,
+    DATE_FORMAT(REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
+FROM JOIN_COUNT
+WHERE REVIEW_COUNT = (
+    SELECT MAX(REVIEW_COUNT)
+    FROM JOIN_COUNT
+)
+ORDER BY REVIEW_DATE ASC, REVIEW_TEXT ASC;
