078dea597b3829150cadf5cc64d268902ade4925
samacyeou <151595973+samacyeou@users.noreply.github.com>
2026-06-26 19:27:35 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 226.99 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 25.4 MB, 시간: 79.61 ms
+메모리: 95.4 MB, 시간: 226.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 19일 12:28:22
+2026년 06월 26일 19:27:31
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,46 +1,29 @@
-import java.util.*; 
-
+import java.util.ArrayList;
+import java.util.Collections;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        int prev;
-        //<"이름", 해당 이름 가진 사람 수>
-        Map<String, Integer> map = new HashMap<>();
+        boolean check = false;
+        ArrayList<String> ac = new ArrayList<String>();
+        ArrayList<String> ap = new ArrayList<String>();
 
-        for(String name : participant){
-            if(map.containsKey(name)) {//이미 있다면
-                prev = map.get(name); //기존값에 
-                map.put(name, prev+1); // +1
-            }else{// 없다면 1로 저장
-                map.put(name, 1); 
-            }
-        }
+        for(int i = 0;i<participant.length;i++)
+            ap.add(participant[i]);
+        for(int i = 0;i<completion.length;i++)
+            ac.add(completion[i]);
 
-        //<"이름", 해당 이름 가진 사람 중 완주한 사람 수>
-        // Map<String, Integer> map2 = new HashMap<>();
+        Collections.sort(ap);
+        Collections.sort(ac);
 
-        for(String name : completion){
-            //해당 키값을 가진 데이터가 있으면 그 값을 가져오고, 없으면 0을 가져옴
-            prev = map.get(name);
-            map.put(name, prev-1); // 가져온 값에 +1
-        }
-
-        for(String name: participant){ //참가자 이름 배열 돌면서
-//             if(!map2.containsKey(name)){ //완주 이름 목록에 없다면
-//                 answer = name; //완주하지 못한 선수!
-//                 break;
-//             }
-
-//             //해당 이름을 가진 사람 수랑 해당 이름을 가지고 완주한 사람 수를 비교
-//             if(map.get(name) != map2.get(name)){
-//                 answer = name; // 다르면 완주하지 못한 선수!
-//                 break;
-//             }
-            if(map.get(name) == 1) {
-                answer = name;
+        for(int j = 0;j<ac.size();j++)
+            if(!ac.get(j).equals(ap.get(j))){
+                answer=ap.get(j);
+                check = true;
                 break;
             }
-        }
+        if(check==false)
+            answer=ap.get(ap.size()-1);
+
 
         return answer;
     }
