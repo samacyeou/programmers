3660520bcb659448ff362282ace04ae61248862d
samacyeou <151595973+samacyeou@users.noreply.github.com>
2026-09-03 19:11:52 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <cmath>
+#include <stdexcept>
+#include <cmath>
+
+using namespace std;
+
+int solution(int a, int b, int c, int d) {
+    int answer = 0;
+    set<int>::iterator iter;
+    set<int>::iterator i;
+    set<int> s={a,b,c,d};
+    if (s.size()==1) {answer=1111*(*s.begin());}
+    else if (s.size()==4) {answer=*s.begin();}
+    else if (s.size()==3) {
+        int temp=1;
+        for(iter=s.begin();iter!=s.end();iter++) temp=temp*(*iter);
+        answer=temp/(a*b*c*d/temp);
+    }
+    else if(s.size()==2) {
+        int temp=1;
+        for(iter=s.begin();iter!=s.end();iter++) temp=temp*(*iter);
+        if(a==b&&b!=c&&c==d || a!=b&&a==c&&b==d || a!=b&&a==d&&b==c) {
+        // if(a*b*c*d/temp/temp==1) {
+            // answer=(*s.begin()+(*s.rbegin()))*(*s.begin()-(*s.rbegin()));
+            if(a!=b) answer = (a+b) * abs(a-b);
+            else answer = (a+c) * abs(a-c);
+        }
+        else answer=pow((10*sqrt(a*b*c*d/temp)+temp/sqrt(a*b*c*d/temp)),2);
+    }
+    return answer;
+}
